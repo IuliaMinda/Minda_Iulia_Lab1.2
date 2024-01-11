a1a738b7472e4d9769d22f390ac6302f48ee452a
Minda Iulia Alexandra <iuliaminda@yahoo.com>
2024-01-11 21:12:08 +0200

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Minda_Iulia_Lab1._2.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
